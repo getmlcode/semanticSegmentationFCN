0efc793d904c0801efe489f948364242c196d3c8
siddharthsvnit <siddharthsvnit@gmail.com>
2019-04-22 21:38:42 +0530

    added learning rate schedule code

--- FCN.py
+++ FCN.py
@@ -10,12 +10,12 @@ class FullyConvNet:
         self.nArgs = len(argv) 
         try:
             if self.nArgs == 6:
-                self.vggModelDir = argv[0]
-                self.trainDataDir = argv[1]
-                self.fcnModelDir = argv[2]
-                self.numClasses = argv[3]
-                self.optAlgo = argv[4]
-                self.learningRate = argv[5]
+                self.vggModelDir      = argv[0]
+                self.trainDataDir     = argv[1]
+                self.fcnModelDir      = argv[2]
+                self.numClasses       = argv[3]
+                self.optAlgo          = argv[4]
+                self.initLearningRate = argv[5]
                 #load/restore Vgg model from vggModelDir and retrieve layers needed for FCN
                 print('Loading VGG model and retrieving layers from : \n{}'.format(self.vggModelDir))
                 self.vggModel = tf.saved_model.loader.load(self.sess, ['vgg16'], self.vggModelDir)
@@ -70,48 +70,41 @@ class FullyConvNet:
         except(ValueError):
             exit('Failed To Construct Object')
 
-    def getOptimizer(loss_op,optAlgo,rate):
+    def getOptimizer(loss_op,optAlgo,rate,global_step):
 
         if optAlgo == 'adam':
-            return tf.train.AdamOptimizer(learning_rate=rate).minimize(loss_op, name="fcn_train_op")
+            return tf.train.AdamOptimizer(rate).minimize(loss_op, global_step=global_step, name="fcn_train_op")
         elif optAlgo == 'grad':
-            return tf.train.GradientDescentOptimizer(learning_rate=rate).minimize(loss_op, name="fcn_train_op")
-
-    #def getRateSchedule(rateSchedule):
-    #    def expDecay(epochNum,alpha,currentRate):
-    #        #implement exponential decay for lerning rate
-    #        return
-
-    #    def fracDecay(epochNum,alpha,currentRate):
-    #        return
-        
-    #    def const(currentRate):
-    #        return currentRate
-
-    #    if rateSchedule == 'exp':
-    #        return expDecay
-    #    elif rateSchedule == 'frac':
-    #        return fracDecay
-    #    else:
-    #        return const
+            return tf.train.GradientDescentOptimizer(rate).minimize(loss_op, global_step=global_step, name="fcn_train_op")
+        elif optAlgo == 'mntm':
+            return tf.train.MomentumOptimizer(rate).minimize(loss_op, global_step=global_step, name="fcn_train_op")
 
     def trainFCN(self):
         try:
             if self.nArgs == 4:
 
-                correct_label = tf.placeholder(tf.float32, [None, IMAGE_SHAPE[0], IMAGE_SHAPE[1], NUMBER_OF_CLASSES])
-                correct_label_reshaped = tf.reshape(correct_label, (-1, num_classes))
+                self.correct_label = tf.placeholder(tf.float32, [None, IMAGE_SHAPE[0], IMAGE_SHAPE[1], self.numClasses])
 
                 # Reshape 4D tensors to 2D, each row represents a pixel, each column a class
                 self.logits = tf.reshape(self.fcn11,(-1, self.numClasses),name="fcn_logits")
-                self.correct_label_reshaped = tf.reshape(correct_label,(-1, num_classes))
+                self.correct_label_reshaped = tf.reshape(self.correct_label,(-1, self.numClasses))
 
                 # Calculate distance from actual labels using cross entropy
                 self.cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=self.logits,
-                                                                        labels=correct_label_reshaped[:])
+                                                                        labels=self.correct_label_reshaped[:])
 
-                self.loss_op = tf.reduce_mean(cross_entropy,name="fcn_loss")
-                self.optimizer = self.getOptimizer(self.loss_op,self.optAlgo,self.learningRate)
+                self.loss_op = tf.reduce_mean(self.cross_entropy,name="fcn_loss")
+                
+                global_step = tf.Variable(0, trainable=False)
+                learning_rate = tf.train.exponential_decay(self.initLearningRate, global_step,100000, 0.96, staircase=True)
+                self.optimizer = self.getOptimizer(self.loss_op,self.optAlgo,learning_rate,global_step)
+                
+                # Refer below links to understand above three lines (hoping they stay alive when you refer it) 
+                # https://stackoverflow.com/questions/33919948/how-to-set-adaptive-learning-rate-for-gradientdescentoptimizer
+                # https://stackoverflow.com/questions/41166681/what-does-global-step-mean-in-tensorflow
+                # https://www.tensorflow.org/api_docs/python/tf/train/exponential_decay
+
+
 
             else:
                 raise ValueError('Object Not Meant For Training')
@@ -133,7 +126,7 @@ if __name__=="__main__":
     fcnModelDir=os.getcwd()+'\\model\\FCN'
 
     print('Creating object for training')
-    fcnImageSegmenter = FullyConvNet(sess,modelDir,trainDir,fcnModelDir,3,'adam',.01)
+    fcnImageSegmenter = FullyConvNet(sess,modelDir,trainDir,fcnModelDir,3,'adam',.1)
     print('Object created successfully')
 
     #fcnImageSegmenter.trainFCN()
