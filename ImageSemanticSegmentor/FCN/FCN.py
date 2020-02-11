dfa73a890dc882c3de554b30b97f8276cc4eafb9
siddharthsvnit <siddharthsvnit@gmail.com>
2020-02-12 02:13:54 +0530

    removed redundant code

--- ImageSemanticSegmentor/FCN/FCN.py
+++ ImageSemanticSegmentor/FCN/FCN.py
@@ -392,26 +392,3 @@ class FullyConvNet:
     
         if metric == 'IOU':
             return semSegMetric.IntersectionOverUnion(validationLabels, predictionLabels, self.numClasses)
-
-if __name__=="__main__":
-    
-    inferSession        = tf.Session()
-
-    inferModelDir       =  os.getcwd()+'\\model\\FCN\\Infer'
-    inferModelName      = 'IOU_0.8870986477691906'
-    ImgSize             = (160,576) # Size(any) to which resize train images
-    numOfClasses        = 2
-
-    print('Creating object for inference')
-    fcnInferImgSegment  = FullyConvNet(inferSession, inferModelDir, inferModelName, ImgSize, 
-                                       numOfClasses)
-    print('Object created successfully')
-
-    image_file          = 'C:\\DataSets\\data_road\\testing\\image_2\\um_000013.png'
-
-    testImage           = scipy.misc.imresize(scipy.misc.imread(image_file), ImgSize)
-    segmentedTestImg    = fcnInferImgSegment.segmentThisImage(testImage)
-    plt.imshow(segmentedTestImg)
-    plt.show()
-
-    inferSession.close()
